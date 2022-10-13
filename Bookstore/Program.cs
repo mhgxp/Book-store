2ff18a3594c7a1b53415f5b40f0c120bd5618e90
mhgxp <86472049+mhgxp@users.noreply.github.com>
2022-10-13 10:12:43 +0700

    Add project files.

---
+++ Bookstore/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFCoreInMemoryDbDemo;
+
+namespace Bookstore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            var app = builder.Build();
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ Bookstore/ValuesController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFCoreInMemoryDbDemo.Models;
+using Microsoft.EntityFrameworkCore;
+namespace EFCoreInMemoryDbDemo
+
+namespace Bookstore
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        readonly IAuthorRepository _authorRepository;
+        public AuthorController(IAuthorRepository authorRepository)
+        {
+            _authorRepository = authorRepository;
+        }
+        [HttpGet]
+        public ActionResult<List<Author>> Get()
+        {
+            return Ok(_authorRepository.GetAuthors());
+        }
+    }
+    public class ApiContext : DbContext
+    {
+        protected override void OnConfiguring
+      (DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "AuthorDb");
+        }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
+    public class Author
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public List<Book> Books { get; set; }
+}
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public Author Author { get; set; }
+}
+public interface IAuthorRepository
+{
+    public List<Author> GetAuthors();
+}
+using EFCoreInMemoryDbDemo.Models;
+using Microsoft.EntityFrameworkCore;
+namespace EFCoreInMemoryDbDemo
+{
+    public class AuthorRepository : IAuthorRepository
+    {
+        public AuthorRepository()
+        {
+            using (var context = new ApiContext())
+            {
+                var authors = new List<Author>
+                {
+                new Author
+                {
+                    FirstName ="Joydip",
+                    LastName ="Kanjilal",
+                       Books = new List<Book>()
+                    {
+                        new Book { Title = "Mastering C# 8.0"},
+                        new Book { Title = "Entity Framework Tutorial"},
+                        new Book { Title = "ASP.NET 4.0 Programming"}
+                    }
+                },
+                new Author
+                {
+                    FirstName ="Yashavanth",
+                    LastName ="Kanetkar",
+                    Books = new List<Book>()
+                    {
+                        new Book { Title = "Let us C"},
+                        new Book { Title = "Let us C++"},
+                        new Book { Title = "Let us C#"}
+                    }
+                }
+                };
+                context.Authors.AddRange(authors);
+                context.SaveChanges();
+            }
+        }
+        public List<Author> GetAuthors()
+        {
+            using (var context = new ApiContext())
+            {
+                var list = context.Authors
+                    .Include(a => a.Books)
+                    .ToList();
+                return list;
+            }
+        }
+    }
+}
